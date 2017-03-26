faf09b4cbb507793ac26bde3d252c5f46645516b
Anshul Gupta <Anshul Gupta>
2017-03-25 23:06:24 -0700

    Add project files.

---
+++ WcfService1/Service1.svc.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+using System.Threading;
+
+namespace WcfService1
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select Service1.svc or Service1.svc.cs at the Solution Explorer and start debugging.
+    [ServiceBehavior(InstanceContextMode = InstanceContextMode.PerSession)]
+    public class Service1 : IService1
+    {
+        double result = 0.0D;
+        string equation;
+
+        public Service1()
+        {
+            equation = result.ToString();
+        }
+
+        public void Clear()
+        {
+            Callback.Equation(equation + " = " + result.ToString());
+            equation = result.ToString();
+        }
+
+        public void AddTo(double n)
+        {
+            result += n;
+            equation += " + " + n.ToString();
+            Callback.Equals(result);
+        }
+
+        public void SubtractFrom(double n)
+        {
+            result -= n;
+            equation += " - " + n.ToString();
+            Callback.Equals(result);
+        }
+
+        public void MultiplyBy(double n)
+        {
+            result *= n;
+            equation += " * " + n.ToString();
+            Callback.Equals(result);
+        }
+
+        public void DivideBy(double n)
+        {
+            result /= n;
+            equation += " / " + n.ToString();
+            Callback.Equals(result);
+        }
+
+        IService1CallBack Callback
+        {
+            get
+            {
+                return OperationContext.Current.GetCallbackChannel<IService1CallBack>();
+            }
+        }
+    }
+
+    
+}
